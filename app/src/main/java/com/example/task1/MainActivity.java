4bbe901efd3487b483e8698ab1517974f6a35904
foxstudent103280 <levitsky2010@gmail.com>
2022-10-29 21:25:17 +0300

    add floating lable

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -20,39 +20,49 @@ import com.google.android.material.textfield.TextInputLayout;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-     String anagram;
+    String anagram;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-                TextInputLayout inputLayoutForAnagram = findViewById(R.id.textInputLayotForAnagram);
+        TextInputLayout inputLayoutForAnagram = findViewById(R.id.textInputLayotForAnagram);
         TextInputEditText editTextForAnagram = findViewById(R.id.editTextForAnagram);
         TextInputEditText filterInputText = findViewById(R.id.filterInputText);
         TextView preViewOfAnagram = findViewById(R.id.preViewOfAnagram);
         TextView myAnagram = findViewById(R.id.myAnagram);
-        Button convertButton = findViewById(R.id.convertButton);
 
         if (savedInstanceState != null) {
             myAnagram.setText(savedInstanceState.getString("myAnagram"));
             preViewOfAnagram.setVisibility(View.INVISIBLE);
             myAnagram.setVisibility(View.VISIBLE);
         }
-//        editTextForAnagram.setText(savedInstanceState.getString("editTextForAnagram"));
-//        filterInputText.setText(savedInstanceState.getString("filterInputText"));
-
 
         editTextForAnagram.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean focusOn) {
-                if (focusOn) {
+                if (focusOn || Objects.requireNonNull(editTextForAnagram.getText()).toString().length() > 0) {
                     inputLayoutForAnagram.setHint(getString(R.string.text_for_anagram));
                     editTextForAnagram.setGravity(Gravity.START);
                 } else {
                     inputLayoutForAnagram.setHint(getString(R.string.enter_text_for_anagram));
                     editTextForAnagram.setGravity(Gravity.CLIP_VERTICAL | Gravity.CENTER_HORIZONTAL);
                     view.requestLayout();
+                    closeKeyboard();
+                }
+            }
+        });
+
+        filterInputText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean focusOn) {
+                if (focusOn || Objects.requireNonNull(filterInputText.getText()).toString().length() > 0) {
+                    filterInputText.setGravity(Gravity.START);
+                } else {
+                    filterInputText.setGravity(Gravity.CLIP_VERTICAL | Gravity.CENTER_HORIZONTAL);
+                    view.requestLayout();
+                    closeKeyboard();
                 }
             }
         });
@@ -65,11 +75,9 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.toString().length() < 1) {
-                    convertButton.setVisibility(View.INVISIBLE);
-                } else {
-                    convertButton.setVisibility(View.VISIBLE);
-                }
+                preViewOfAnagram.setVisibility(View.INVISIBLE);
+                myAnagram.setVisibility(View.VISIBLE);
+                myAnagram.setText(convertToAnagram(Objects.requireNonNull(editTextForAnagram.getText()).toString(), Objects.requireNonNull(filterInputText.getText()).toString()));
             }
 
             @Override
@@ -78,27 +86,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        filterInputText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        filterInputText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onFocusChange(View view, boolean focusOn) {
-                if (focusOn) {
-                    filterInputText.setGravity(Gravity.START);
-                } else {
-                    filterInputText.setGravity(Gravity.CLIP_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                    view.requestLayout();
-                    filterInputText.clearFocus();
-                }
-            }
-        });
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-        convertButton.setOnClickListener(new View.OnClickListener() {
+            }
+
             @Override
-            public void onClick(View view) {
-                preViewOfAnagram.setVisibility(View.INVISIBLE);
-                myAnagram.setVisibility(View.VISIBLE);
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
                 myAnagram.setText(convertToAnagram(Objects.requireNonNull(editTextForAnagram.getText()).toString(), Objects.requireNonNull(filterInputText.getText()).toString()));
-                closeKeyboard();
-                anagram = myAnagram.getText().toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
@@ -188,8 +189,6 @@ public class MainActivity extends AppCompatActivity {
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
 
-//        outState.putString("editTextForAnagram", Objects.requireNonNull(editTextForAnagram.getText()).toString());
-//        outState.putString("filterInputText", Objects.requireNonNull(filterInputText.getText()).toString());
         outState.putString("myAnagram", anagram);
     }
 }
