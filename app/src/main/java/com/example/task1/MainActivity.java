abf03d49d30d6c492c3873eff210c2c29def8220
Oleksandr Levytskyi <levitsky2010@gmail.com>
2022-11-26 02:18:09 +0200

    First changes after Dan comments

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -1,29 +1,17 @@
 package com.example.task1;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Gravity;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
-    /*
-     *variables which should be visible through whole the scope
-     */
-    int editTextLength;
-    int filterLength;
-    String textForAnagram;
-    String textForFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,95 +27,12 @@ public class MainActivity extends AppCompatActivity {
         /**
          * link between views from xml and logic of program
          */
-        TextInputLayout inputLayoutForAnagram = findViewById(R.id.textInputLayotForAnagram);
         TextInputEditText editTextForAnagram = findViewById(R.id.editTextForAnagram);
         TextInputEditText filterInputText = findViewById(R.id.filterInputText);
         TextView preViewOfAnagram = findViewById(R.id.preViewOfAnagram);
         TextView myAnagram = findViewById(R.id.myAnagram);
 
-        /**
-         * redefining state of view after rotation
-         */
-        if (savedInstanceState != null) {
-            preViewOfAnagram.setVisibility(View.INVISIBLE);
-            myAnagram.setVisibility(View.VISIBLE);
-            editTextLength = savedInstanceState.getInt("lengthOfText");
-            filterLength = savedInstanceState.getInt("lengthOfFilter");
-
-            if (editTextLength > 0) {
-                editTextForAnagram.setGravity(Gravity.START);
-                editTextForAnagram.setTextSize(22);
-
-            } else {
-                editTextForAnagram.setGravity(Gravity.CLIP_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                editTextForAnagram.setTextSize(16);
-            }
-
-            if (filterLength > 0) {
-                filterInputText.setGravity(Gravity.START);
-                filterInputText.setTextSize(22);
-            } else {
-                filterInputText.setGravity(Gravity.CLIP_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                filterInputText.setTextSize(16);
-            }
-        }
-
-        /**
-         * change state of view editTextForAnagram despite of focus
-         */
-        editTextForAnagram.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean focusOn) {
-
-                if (textForAnagram == null) {
-                    editTextLength = 0;
-                } else {
-                    editTextLength = textForAnagram.length();
-                }
-
-                if (focusOn || editTextLength > 0) {
-                    editTextForAnagram.setTextSize(22);
-                    inputLayoutForAnagram.setHint(getString(R.string.text_for_anagram));
-                    editTextForAnagram.setGravity(Gravity.START);
-                } else {
-                    editTextForAnagram.setTextSize(16);
-                    inputLayoutForAnagram.setHint(getString(R.string.enter_text_for_anagram));
-                    editTextForAnagram.setGravity(Gravity.CLIP_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                    view.requestLayout();
-                    closeKeyboard();
-                }
-            }
-        });
-
-        /**
-         * change state of view filterInputText despite of focus
-         */
-        filterInputText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean focusOn) {
-
-                if (textForFilter == null) {
-                    filterLength = 0;
-                } else {
-                    filterLength = textForFilter.length();
-                }
-
-                if (focusOn || filterLength > 0) {
-                    filterInputText.setGravity(Gravity.START);
-                    filterInputText.setTextSize(22);
-                } else {
-                    filterInputText.setGravity(Gravity.CLIP_VERTICAL | Gravity.CENTER_HORIZONTAL);
-                    filterInputText.setTextSize(16);
-                    view.requestLayout();
-                    closeKeyboard();
-                }
-            }
-        });
-
-        /**
-         * listen of text input and sent each symbol to method in TextConvertToAnagram
-         */
-        editTextForAnagram.addTextChangedListener(new TextWatcher() {
+        TextWatcher textWatcher = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -137,56 +42,24 @@ public class MainActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 preViewOfAnagram.setVisibility(View.INVISIBLE);
                 myAnagram.setVisibility(View.VISIBLE);
-                textForAnagram = editTextForAnagram.getText().toString();
-                textForFilter = String.valueOf(filterInputText.getText());
-                myAnagram.setText(TextConvertToAnagram.convertToAnagram(textForAnagram, textForFilter));
+                myAnagram.setText(TextConvertToAnagram.convertToAnagram(editTextForAnagram.getText().toString(), filterInputText.getText().toString()));
             }
 
             @Override
             public void afterTextChanged(Editable s) {
 
             }
-        });
+        };
+
+        /**
+         * listen of text input and sent each symbol to method in TextConvertToAnagram
+         */
+        editTextForAnagram.addTextChangedListener(textWatcher);
 
         /**
          * listen of filter input and sent each symbol to method in TextConvertToAnagram to correct anagram according to filter
          */
-        filterInputText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        filterInputText.addTextChangedListener(textWatcher);
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                textForFilter = filterInputText.getText().toString();
-                textForAnagram = String.valueOf(editTextForAnagram.getText());
-                myAnagram.setText(TextConvertToAnagram.convertToAnagram(textForAnagram, textForFilter));
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-    }
-
-    /**
-     * hide keyboard
-     */
-    protected void closeKeyboard() {
-        View view = this.getCurrentFocus();
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        outState.putInt("lengthOfText", editTextLength);
-        outState.putInt("lengthOfFilter", filterLength);
     }
 }
--- app/src/main/java/com/example/task1/TextConvertToAnagram.java
+++ app/src/main/java/com/example/task1/TextConvertToAnagram.java
@@ -6,63 +6,58 @@ public class TextConvertToAnagram {
      * start a process of converting - split text for anagram into individual words and create array of words
      * @return built anagram
      */
-    public static String convertToAnagram(String textForAnagram, String filterForAnagram) {
-        String[] wordsForAnagram = textForAnagram.split(" ");
-        symbolsInWordsReverse(wordsForAnagram, filterForAnagram);
+    public static String convertToAnagram(String text, String filter) {
+        String[] words = text.split("\\s+");
+        symbolsInWordsReverse(words, filter);
 
-        return buildOfAnagram(wordsForAnagram);
+        return buildOfAnagram(words);
     }
 
     /**
-     * loop which direct each word to reverse
-     */
-    public static void symbolsInWordsReverse(String[] words, String filter) {
-        for (int i = 0; i < words.length; i++) {
-            words[i] = symbolsReverse(words[i], filter);
-        }
-    }
-
-    /**
+     * loop directs each word to reverse
      * reverse each word taking into account filter symbols - keep position of symbols from filter
      * check each symbol - doesn't take into account digits and non alphabetic symbols
      * @return reversed and filtered word
      */
-    public static String symbolsReverse(String word, String filter) {
-        char[] symbols = word.toCharArray();
+    private static void symbolsInWordsReverse(String[] words, String filter) {
+        for (int k = 0; k < words.length; k++) {
+            char[] symbols = words[k].toCharArray();
 
-        if (filter.isEmpty()) {
-            for (int i = 0, j = symbols.length - 1; i < j; i++, j--) {
-                while (Character.isAlphabetic(symbols[i]) && i < j) {
-                    i++;
+            if (filter.isEmpty()) {
+                for (int i = 0, j = symbols.length - 1; i < j; i++, j--) {
+                    if (!Character.isAlphabetic(symbols[i])) {
+                        i++;
+                    }
+                    if (!Character.isAlphabetic(symbols[i]) && j > i) {
+                        j--;
+                    }
+                    char tmp = symbols[i];
+                    symbols[i] = symbols[j];
+                    symbols[j] = tmp;
                 }
-                while (Character.isAlphabetic(symbols[i]) && j > i) {
-                    j--;
+            } else {
+                for (int i = 0, j = symbols.length - 1; i < j; i++, j--) {
+                    if (filterCheck(symbols[i], filter.toCharArray())) {
+                        i++;
+                    }
+                    if (filterCheck(symbols[j], filter.toCharArray())) {
+                        j--;
+                    }
+                    char tmp = symbols[i];
+                    symbols[i] = symbols[j];
+
+                    symbols[j] = tmp;
                 }
-                char tmp = symbols[i];
-                symbols[i] = symbols[j];
-                symbols[j] = tmp;
             }
-        } else {
-            for (int i = 0, j = symbols.length - 1; i < j; i++, j--) {
-                while (filterCheck(symbols[i], filter.toCharArray()) && i < j) {
-                    i++;
-                }
-                while (filterCheck(symbols[j], filter.toCharArray()) && j > i) {
-                    j--;
-                }
-                char tmp = symbols[i];
-                symbols[i] = symbols[j];
-                symbols[j] = tmp;
-            }
+            words[k] = new String(symbols);
         }
-        return new String(symbols);
     }
 
     /**
      * checking symbols - comparison with symbols from filter
      * @return true symbol anf filter are equal and false if no
      */
-    public static boolean filterCheck(char symbolToCheck, char[] filterSymbols) {
+    private static boolean filterCheck(char symbolToCheck, char[] filterSymbols) {
         for (char filterSymbol : filterSymbols) {
             if (filterSymbol == symbolToCheck) {
                 return true;
@@ -75,7 +70,7 @@ public class TextConvertToAnagram {
      * building anagram from reversed words
      * @return built anagram
      */
-    public static String buildOfAnagram(String[] wordsAfterReverse) {
+    private static String buildOfAnagram(String[] wordsAfterReverse) {
         StringBuilder resultString = new StringBuilder();
         int lastIndex = wordsAfterReverse.length - 1;
 
