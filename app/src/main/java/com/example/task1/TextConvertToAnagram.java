3eee6ef384cbfb4e663726c8d025e09dd1863627
Oleksandr Levytskyi <levitsky2010@gmail.com>
2022-11-30 18:34:29 +0200

    Adapted method filterCheck to get char as argument instead String

--- app/src/main/java/com/example/task1/TextConvertToAnagram.java
+++ app/src/main/java/com/example/task1/TextConvertToAnagram.java
@@ -46,10 +46,10 @@ public class TextConvertToAnagram {
                 }
             } else {
                 for (int i = 0, j = symbols.length - 1; i < j; i++, j--) {
-                    if (filterCheck(String.valueOf(symbols[i]), filter)) {
+                    if (filterCheck(symbols[i], filter)) {
                         i++;
                     }
-                    if (filterCheck(String.valueOf(symbols[j]), filter)) {
+                    if (filterCheck(symbols[j], filter)) {
                         j--;
                     }
                     char tmp = symbols[i];
@@ -66,7 +66,7 @@ public class TextConvertToAnagram {
      * checking symbols - comparison with symbols from filter
      * @return true symbol anf filter are equal and false if no
      */
-    private static boolean filterCheck(String symbolToCheck, String filter) {
-        return filter.contains(symbolToCheck);
+    private static boolean filterCheck(char symbolToCheck, String filter) {
+        return filter.lastIndexOf(symbolToCheck) != -1;
     }
 }
