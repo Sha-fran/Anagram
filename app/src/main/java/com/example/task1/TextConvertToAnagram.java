7e3f1fb9efa25b9d09515f17303a6bc8512fdda8
Oleksandr Levytskyi <levitsky2010@gmail.com>
2022-12-02 19:02:00 +0200

    move symbols checking from symbolsInWordsReverse to filterCheck

--- app/src/main/java/com/example/task1/TextConvertToAnagram.java
+++ app/src/main/java/com/example/task1/TextConvertToAnagram.java
@@ -16,7 +16,7 @@ public class TextConvertToAnagram {
             anagrama.append(symbolsInWordsReverse(words[i], filter)).append(" ");
         }
         anagrama.append(symbolsInWordsReverse(words[lastIndex], filter));
-        
+
         return anagrama.toString();
     }
 
@@ -25,34 +25,27 @@ public class TextConvertToAnagram {
      * reverse each word taking into account filter symbols - keep position of symbols from filter
      * check each symbol - doesn't take into account digits and non alphabetic symbols
      * building anagram from reversed words     *
+     *
      * @return built anagram
      */
     private static String symbolsInWordsReverse(String word, String filter) {
-            char[] symbols = word.toCharArray();
+        char[] symbols = word.toCharArray();
 
-            for (int i = 0, j = symbols.length - 1; i < j; i++, j--) {
-                if (filter.isEmpty()) {
-                    if (!Character.isAlphabetic(symbols[i])) {
-                        i++;
-                    }
-                    if (!Character.isAlphabetic(symbols[i]) && j > i) {
-                        j--;
-                    }
-
-                } else {
-                    if (filterCheck(symbols[i], filter)) {
-                        i++;
-                    }
-                    if (filterCheck(symbols[j], filter)) {
-                        j--;
-                    }
-                }
-                char tmp = symbols[i];
-                symbols[i] = symbols[j];
-                symbols[j] = tmp;
+        for (int i = 0, j = symbols.length - 1; i < j; i++, j--) {
+            if (filterCheck(symbols[i], filter)) {
+                i++;
             }
+            ;
+            if (filterCheck(symbols[j], filter)) {
+                j--;
+            };
+
+            char tmp = symbols[i];
+            symbols[i] = symbols[j];
+            symbols[j] = tmp;
+        }
         word = new String(symbols);
-            
+
         return word;
     }
 
@@ -62,6 +55,6 @@ public class TextConvertToAnagram {
      * @return true symbol anf filter are equal and false if no
      */
     private static boolean filterCheck(char symbolToCheck, String filter) {
-        return filter.lastIndexOf(symbolToCheck) != -1;
+        return !Character.isAlphabetic(symbolToCheck) || filter.lastIndexOf(symbolToCheck) != -1;
     }
 }
