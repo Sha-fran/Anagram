9ef42d8abe7f03d7f3a028ec7a38df25acef50c3
Oleksandr Levytskyi <levitsky2010@gmail.com>
2023-02-20 21:44:25 +0200

    UI tests final edits

--- app/src/androidTest/java/com/example/task1/FieldsForUITests.java
+++ app/src/androidTest/java/com/example/task1/FieldsForUITests.java
@@ -1,20 +1,13 @@
 package com.example.task1;
 
 public class FieldsForUITests {
-        public  String input;
-        public  String filter;
-        public  String expectedOutput;
+        public final String input;
+        public final String filter;
+        public final String expectedOutput;
 
         FieldsForUITests(String input, String filter, String expectedOutput) {
                 this.input = input;
                 this.filter = filter;
                 this.expectedOutput = expectedOutput;
         }
-
-
-//        Integer.valueOf(R.id.editTextForAnagram);
-//        Integer.valueOf(R.id.filterInputText);
-//        Integer.valueOf(R.id.preViewOfAnagram);
-//        Integer.valueOf(R.id.myAnagram);
-
 }
