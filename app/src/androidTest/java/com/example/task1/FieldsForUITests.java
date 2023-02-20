50e75403baa1cf01f669acd61169d9c067704dec
Oleksandr Levytskyi <levitsky2010@gmail.com>
2023-02-20 21:34:49 +0200

    UI tests +perform tests after explanation

---
+++ app/src/androidTest/java/com/example/task1/FieldsForUITests.java
@@ -0,0 +1,20 @@
+package com.example.task1;
+
+public class FieldsForUITests {
+        public  String input;
+        public  String filter;
+        public  String expectedOutput;
+
+        FieldsForUITests(String input, String filter, String expectedOutput) {
+                this.input = input;
+                this.filter = filter;
+                this.expectedOutput = expectedOutput;
+        }
+
+
+//        Integer.valueOf(R.id.editTextForAnagram);
+//        Integer.valueOf(R.id.filterInputText);
+//        Integer.valueOf(R.id.preViewOfAnagram);
+//        Integer.valueOf(R.id.myAnagram);
+
+}
--- app/src/androidTest/java/com/example/task1/MaiActivityTest.java
+++ app/src/androidTest/java/com/example/task1/MaiActivityTest.java
@@ -1,26 +1,13 @@
 package com.example.task1;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.clearText;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.action.ViewActions.typeTextIntoFocusedView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isFocusable;
-import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.google.android.material.textfield.TextInputEditText;
-
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -30,34 +17,18 @@ import java.util.List;
 @RunWith(AndroidJUnit4.class)
 
 public class MaiActivityTest {
-    private final List<Integer> uiTestCases = new ArrayList<Integer>() {{
-        Integer.valueOf(R.id.editTextForAnagram);
-        Integer.valueOf(R.id.filterInputText);
-        Integer.valueOf(R.id.preViewOfAnagram);
-        Integer.valueOf(R.id.myAnagram);
-    }};
-
-    private final List<Integer> uiTestActiveCases = new ArrayList<Integer>() {{
-        Integer.valueOf(R.id.editTextForAnagram);
-        Integer.valueOf(R.id.filterInputText);
+    private final List<FieldsForUITests> uiTestCases = new ArrayList<FieldsForUITests>() {{
+        new FieldsForUITests("Foxminded cool 24/7", "xl", "dexdnimoF oocl 7/42");
+        new FieldsForUITests("", "xl", "");
+        new FieldsForUITests("Foxminded cool 24/7", "", "dednimxoF looc 24/7");
     }};
 
     @Test
     public void desctopElementIsDisplayed() {
-        for (int cases : uiTestCases) {
-            onView(withId(cases)).check(matches(isDisplayed()));
-        }
-    }
-
-    @Test
-    public void activeDesctopElementActions() {
-        for (int cases : uiTestActiveCases) {
-            onView(withId(cases)).check(matches(isFocusable()));
-            onView(withId(cases)).check(matches(isClickable()));
-            onView(withId(cases)).perform(typeTextIntoFocusedView("Hello!"));
-            onView(withId(cases)).check(matches(withText("Hello!")));
-            onView(withId(cases)).perform(click(), clearText(), typeText("How are you?"), closeSoftKeyboard());
-            onView(withId(R.id.myAnagram)).check(matches(withText("How are you?")));
+        for (FieldsForUITests cases : uiTestCases) {
+            onView(withId(R.id.editTextForAnagram)).perform(typeTextIntoFocusedView(cases.input));
+            onView(withId(R.id.filterInputText)).perform(typeTextIntoFocusedView(cases.filter));
+            onView(withId(R.id.myAnagram)).check(matches(withText(cases.expectedOutput)));
         }
     }
 }
