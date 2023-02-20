988d135564872b25a7fe8fb8d1416561039979f7
Oleksandr Levytskyi <levitsky2010@gmail.com>
2023-02-20 20:22:35 +0200

    UI tests +perform tests

--- app/src/androidTest/java/com/example/task1/MaiActivityTest.java
+++ app/src/androidTest/java/com/example/task1/MaiActivityTest.java
@@ -1,8 +1,16 @@
 package com.example.task1;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.action.ViewActions.typeTextIntoFocusedView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isFocusable;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -29,10 +37,27 @@ public class MaiActivityTest {
         Integer.valueOf(R.id.myAnagram);
     }};
 
+    private final List<Integer> uiTestActiveCases = new ArrayList<Integer>() {{
+        Integer.valueOf(R.id.editTextForAnagram);
+        Integer.valueOf(R.id.filterInputText);
+    }};
+
     @Test
-    public void editTextForAnagramIsDisplayed() {
+    public void desctopElementIsDisplayed() {
         for (int cases : uiTestCases) {
             onView(withId(cases)).check(matches(isDisplayed()));
+        }
+    }
+
+    @Test
+    public void activeDesctopElementActions() {
+        for (int cases : uiTestActiveCases) {
+            onView(withId(cases)).check(matches(isFocusable()));
+            onView(withId(cases)).check(matches(isClickable()));
+            onView(withId(cases)).perform(typeTextIntoFocusedView("Hello!"));
+            onView(withId(cases)).check(matches(withText("Hello!")));
+            onView(withId(cases)).perform(click(), clearText(), typeText("How are you?"), closeSoftKeyboard());
+            onView(withId(R.id.myAnagram)).check(matches(withText("How are you?")));
         }
     }
 }
